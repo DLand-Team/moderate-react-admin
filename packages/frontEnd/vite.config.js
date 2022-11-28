4f3147f43376b1c4d9e7ffe8edef325bdfc263fa
qanglee <545540710@qq.com>
2022-11-28 17:00:53 +0800

    fix: 调整服务端口号

--- packages/frontEnd/vite.config.js
+++ packages/frontEnd/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3120/',
+        target: 'http://localhost:3130/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
