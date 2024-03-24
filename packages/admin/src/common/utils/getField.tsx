d3473875655638d097ae35c8f08134ec4a2a0c69
qanglee <545540710@qq.com>
2024-03-24 20:32:23 +0800

    dev

--- packages/admin/src/common/utils/getField.tsx
+++ packages/admin/src/common/utils/getField.tsx
@@ -84,8 +84,10 @@ export const Field = <T = any,>({
 	const record = useRef<T>();
 	const watchState = Form.useWatch((values) => values, formIns);
 	useEffect(() => {
-		watch?.(watchState, record.current!);
-		record.current = formIns.getFieldsValue();
+		if (watch) {
+			watch?.(watchState, record.current!);
+			record.current = formIns.getFieldsValue();
+		}
 	}, [watchState]);
 	if (render) {
 		return render(fieldConfig, formIns);
