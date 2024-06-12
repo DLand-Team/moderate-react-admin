6c8cb78e8c8fa9e194a7a4e6d0fcefc326ac5b7d
qanglee <545540710@qq.com>
2024-06-12 14:51:16 +0800

    dev

--- frontend/packages/admin/plugins/config/providers.ts
+++ frontend/packages/admin/plugins/config/providers.ts
@@ -3,6 +3,6 @@ import { WinBoxProvider } from "plugins/moderate-plugin-winbox/providers/winBoxP
 //>>>PROVIDER_INPORT_SIGN<<<//
 
 export const pluginProviders = [
-  //>>>PROVIDER_SIGN<<<//
-  WinBoxProvider,
+	WinBoxProvider,
+	//>>>PROVIDER_SIGN<<<//
 ];
