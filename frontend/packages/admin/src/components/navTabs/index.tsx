06d65ee08f92b5392ab6d920176c7bd55e3f78cf
qanglee <545540710@qq.com>
2024-06-12 14:53:19 +0800

    dev

--- frontend/packages/admin/src/components/navTabs/index.tsx
+++ frontend/packages/admin/src/components/navTabs/index.tsx
@@ -180,11 +180,13 @@ const App: React.FC = () => {
 				}
 			}
 			let temp = tabItemsTemp.map((item) => {
-				const metaTitle = RouterHelper.getRoutItemConfigByPath(
-					item.location?.pathname!,
-				).meta?.title;
-				if (metaTitle) {
-					item.label = t(metaTitle);
+				if (item.location) {
+					const metaTitle = RouterHelper.getRoutItemConfigByPath(
+						item.location?.pathname!,
+					)?.meta?.title;
+					if (metaTitle) {
+						item.label = t(metaTitle);
+					}
 				}
 				return item;
 			});
