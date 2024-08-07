724816bc63f89948031cde1b7a4b42005cb6ec93
qanglee <545540710@qq.com>
2024-08-07 16:36:11 +0800

    dev

--- frontend/packages/dev-server/src/helper/pathHelper.ts
+++ frontend/packages/dev-server/src/helper/pathHelper.ts
@@ -4,32 +4,38 @@ export default {
 	src: path.resolve("src"),
 	apisPath: path.resolve("src/apis"),
 	pluginsCache: path.resolve("./pluginsCahche"),
-	adminPlugins: path.resolve("../admin/plugins"),
-	webPath: path.resolve("../admin"),
-	webSrcPath: path.resolve("../admin/src"),
-	webRouterConfigPath: path.resolve("../admin/src/router/routesConfig.ts"), // 路由配置数据
-	webRouterStructPath: path.resolve("../admin/src/router/routesTree.ts"), // 路由树结构
-	webRouterNamePath: path.resolve("../admin/src/router/name.ts"), // 路由名称
+	adminPlugins: path.resolve("../admin-antd/plugins"),
+	webPath: path.resolve("../admin-antd"),
+	webSrcPath: path.resolve("../admin-antd/src"),
+	webRouterConfigPath: path.resolve(
+		"../admin-antd/src/router/routesConfig.ts",
+	), // 路由配置数据
+	webRouterStructPath: path.resolve("../admin-antd/src/router/routesTree.ts"), // 路由树结构
+	webRouterNamePath: path.resolve("../admin-antd/src/router/name.ts"), // 路由名称
 	pluginRouterConfigPath: path.resolve(
-		"../admin/plugins/config/router/routesConfig.ts",
+		"../admin-antd/plugins/config/router/routesConfig.ts",
 	), // 路由配置数据
 	pluginRouterStructPath: path.resolve(
-		"../admin/plugins/config/router/routesTree.ts",
+		"../admin-antd/plugins/config/router/routesTree.ts",
 	), // 路由树结构
 	pluginRouterNamePath: path.resolve(
-		"../admin/plugins/config/router/name.ts",
+		"../admin-antd/plugins/config/router/name.ts",
 	), // 路由名称
-	providerConfigPath: path.resolve("../admin/plugins/config/providers.ts"), // provider配置数据
-	themeConfigPath: path.resolve("../admin/plugins/config/themes.ts"), // theme主题
-	layoutConfigPath: path.resolve("../admin/plugins/config/layouts.ts"), // 布局主题
-	webLocalesPath: path.resolve("../admin/plugins/config/i18n/locales"),
-	webPagesPath: path.resolve("../admin/src/pages"),
-	webServicePath: path.resolve("../admin/src/services"),
-	webStoresPath: path.resolve("../admin/src/services/stores"),
-	webStoresIndexPath: path.resolve("../admin/src/services/stores/index.ts"),
-	webApisPath: path.resolve("../admin/src/services/apis"),
-	webApisIndexPath: path.resolve("../admin/src/services/apis/index.ts"),
-	webPageIndexPath: path.resolve("../admin/src/pages/index.tsx"),
-	pluginPageIndexPath: path.resolve("../admin/plugins/config/pages.ts"),
-	webSettingPath: path.resolve("../admin/src/setting.json"),
+	providerConfigPath: path.resolve(
+		"../admin-antd/plugins/config/providers.ts",
+	), // provider配置数据
+	themeConfigPath: path.resolve("../admin-antd/plugins/config/themes.ts"), // theme主题
+	layoutConfigPath: path.resolve("../admin-antd/plugins/config/layouts.ts"), // 布局主题
+	webLocalesPath: path.resolve("../admin-antd/plugins/config/i18n/locales"),
+	webPagesPath: path.resolve("../admin-antd/src/pages"),
+	webServicePath: path.resolve("../admin-antd/src/services"),
+	webStoresPath: path.resolve("../admin-antd/src/services/stores"),
+	webStoresIndexPath: path.resolve(
+		"../admin-antd/src/services/stores/index.ts",
+	),
+	webApisPath: path.resolve("../admin-antd/src/services/apis"),
+	webApisIndexPath: path.resolve("../admin-antd/src/services/apis/index.ts"),
+	webPageIndexPath: path.resolve("../admin-antd/src/pages/index.tsx"),
+	pluginPageIndexPath: path.resolve("../admin-antd/plugins/config/pages.ts"),
+	webSettingPath: path.resolve("../admin-antd/src/setting.json"),
 };
